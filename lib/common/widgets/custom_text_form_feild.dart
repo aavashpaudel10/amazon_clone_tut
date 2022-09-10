dcc8cf26d7af349ee4f628c1cad9818f1abe5dcc
withcreater <aavashpaudel10@gmail.com>
2022-09-10 13:15:05 +0545

    ui login

---
+++ lib/common/widgets/custom_text_form_feild.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomeTextFormFeild extends StatelessWidget {
+  const CustomeTextFormFeild({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: ,
+      decoration: InputDecoration(),
+
+    );
+  }
+}
